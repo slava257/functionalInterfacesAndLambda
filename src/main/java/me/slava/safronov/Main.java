14ecf338f71aac6bea5abd31ab0e724b03b82e47
79650 <last137954682@gmail.com>
2023-06-23 19:24:14 +0300

    Initial commit

---
+++ src/main/java/me/slava/safronov/Main.java
@@ -0,0 +1,5 @@
+package me.slava.safronov;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/me/slava/safronov/Stocks.java
@@ -0,0 +1,2 @@
+package me.slava.safronov;public class Stocks {
+}
